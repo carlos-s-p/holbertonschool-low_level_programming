7a370476daf61612dff11b34db8e603b85bfff6c
Carlos Serrano <cserrano5880@gmail.com>
2021-10-01 16:27:14 +0000

    3-print_alphabets.c file added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet lowercase and UPPERCASE
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	int nm = 65;
+
+	while ((n >= 97) && (n <= 122))
+	{
+	putchar(n);
+	n++;
+	}
+	while ((nm >= 65) && (nm <= 90))
+	{
+	putchar(nm);
+	nm++;
+	}
+	putchar(13) ;
+	return (0);
+}
