594d8b1f154a7788d8702c88ffd8cfdc6c601e4e
Carlos Serrano <cserrano5880@gmail.com>
2021-10-01 16:12:06 +0000

    2-print_alphabet.c file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while ((n >= 97) && (n <= 122))
+	{
+	putchar(n);
+	n++;
+	}
+	printf("\n");
+	return (0);
+}
+
