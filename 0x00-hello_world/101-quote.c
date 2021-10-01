be0e978caec32c82e5cbb28cf342c4fa810faa51
Carlos Serrano <cserrano5880@gmail.com>
2021-10-01 04:50:28 +0000

    101-quote file added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints the message "and that...." to the stderr-> standard error
+ * main - dPrints the message "and that...." to the stderr-> standard error
  *
  * Return: Returns 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	dprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
