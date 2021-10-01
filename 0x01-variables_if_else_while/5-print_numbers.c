51fa5b7b18cd368a44fd6965b296c28bc980f4d0
Carlos Serrano <cserrano5880@gmail.com>
2021-10-01 16:54:49 +0000

    5-print_numbers.c file added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the digits base 10
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while ((n >= 48) && (n <= 57))
+	{
+	putchar(n);
+	n++;
+	}
+	putchar(13);
+	return (0);
+}
+
