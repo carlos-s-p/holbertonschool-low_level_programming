7d8557c7871e66bc6c1649be8276e608a6cb45d5
Carlos Serrano <cserrano5880@gmail.com>
2021-10-01 23:02:48 +0000

    101-print_comb4.c file added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints combinations of three digits, not repeated 
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+
+	int j = 48;
+
+	int i;
+
+	int a;
+
+	while (j < 57)
+	{
+	i = j;
+	while (i <= 57)
+	{
+	if (i != j)
+	{
+	a = i;
+	while (a <= 57)
+	{
+
+	if ((j != i) && (a != i))
+	{
+	putchar(j);
+	putchar(i);
+	putchar(a);
+
+	if (j <= 54)
+	{
+	putchar(32);
+	putchar(44);
+	}
+
+	}
+	a++;
+	}
+	}
+	i++;
+	}
+	j++;
+	}
+	putchar(13);
+	return (0);
+}
