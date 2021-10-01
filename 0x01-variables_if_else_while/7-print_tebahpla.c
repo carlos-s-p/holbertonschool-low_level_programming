34cd1fadd022c66ef9e3536878ab02729fff46a8
Carlos Serrano <cserrano5880@gmail.com>
2021-10-01 17:08:02 +0000

    7-print_tebahpla.c file added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet reverse
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while ((n >= 97))
+	{
+	putchar(n);
+	n--;
+	}
+	putchar(13);
+	return (0);
+}
+
