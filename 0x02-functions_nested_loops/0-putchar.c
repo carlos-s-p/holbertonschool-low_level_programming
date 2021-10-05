e7a393185b1e25383c490e766547b82e240baff1
Carlos Serrano <cserrano5880@gmail.com>
2021-10-05 22:08:37 +0000

    file 0-putchar.c added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Prints the word _putchar
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# NESTED-LOOPS!
+
+In this project we will show how to use nested loops and headers..
+
+
+# Files
+Current files:
+**online!**
