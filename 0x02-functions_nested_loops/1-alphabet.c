3a935527fd0700d6f874fb1da51bd22fbb880c6c
Carlos Serrano <cserrano5880@gmail.com>
2021-10-05 22:17:59 +0000

    file 1-alphabet added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+	_putchar(i);
+	}
+	_putchar(10);
+
+	return (0);
+
+
+}
