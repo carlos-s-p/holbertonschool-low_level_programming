ed492ab5b2d5a07cc840240e333776230ba4de95
Carlos Serrano <cserrano5880@gmail.com>
2021-10-06 03:09:56 +0000

    11-print_to_98.c file added and main.h updated

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include <"stdio.h">
+int _putchar(char c);
+/**
+* print_to_98 - function that evaluates the values to 98
+* @n: parameter integer type
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	int direc;
+
+	int diff;
+
+	diff = (98 - n);
+
+	if (diff > 0)
+	{
+	direc = 1;
+	}
+	else if (diff < 0)
+	{
+	direc = -1;
+
+	}
+	else
+	{
+	direc = 0;
+
+	}
+	i = n;
+	while (i != 98)
+	{
+	printf("%d, ", i);
+
+		if (direc == 1)
+		{
+		i++;
+		}
+		if (direc == -1)
+		{
+		i--;
+		}
+	}
+
+	printf("%d \n", i);
+
+}
+
