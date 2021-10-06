bedda94fbf7c24c3f56f4ebf61e0dc541e8f5e41
Carlos Serrano <cserrano5880@gmail.com>
2021-10-06 00:58:58 +0000

    main.h updated and 3-islower.c fixed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+int _putchar(char c);
+/**
+ * _islower- function receives a value and veryfi if its lower
+ * @c: Parameter of the function (integer)
+ * Return: return 0 or 1 depending or is lower or not
+ *
+ */
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
