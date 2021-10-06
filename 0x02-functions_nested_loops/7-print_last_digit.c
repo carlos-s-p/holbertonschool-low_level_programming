f653fd804111feb101f1d565721db4163588c852
Carlos Serrano <cserrano5880@gmail.com>
2021-10-06 01:51:23 +0000

    7-print_last_digit.c file updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,21 @@
 int _putchar(char c);
-int _abs(int n);
+
+/**
+* _abs - function that evaluates absolute value
+* @n: parameter n integer
+* Return: returns absolute value of n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	n *= (-1);
+
+	}
+	return (n);
+
+}
+
 /**
 * print_last_digit - function that evaluates last digit
 * @n: parameter n integer
