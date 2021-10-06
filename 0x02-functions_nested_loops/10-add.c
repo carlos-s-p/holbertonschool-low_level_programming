a6c108cdc756aaaf8f76a5b8832bfec3ff853078
Carlos Serrano <cserrano5880@gmail.com>
2021-10-06 02:53:39 +0000

    file 10-add.c added and main.h updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+int _putchar(char c);
+/**
+* add - function that evaluates the sum of two integer values
+* @a: parameter a integer
+* @b: parameter b integer
+* Return: returns absolute value of n
+*/
+int add(int a, int b)
+{
+	return (a + b);
+
+}
