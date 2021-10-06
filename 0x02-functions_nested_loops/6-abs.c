a15cd37a6491a80a4f9d9c4f5c7b6888b626fb48
Carlos Serrano <cserrano5880@gmail.com>
2021-10-06 01:37:56 +0000

    file main.h updated and 6-abs.c added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+int _putchar(char c);
+/**
+* _abs - function that evaluates absolute value
+* @n: parameter n integer
+* Return: returns absolute value of n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	n *= (-1);
+	}
+return (n);
+
+}
